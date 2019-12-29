f243831b9653cc625901a31a8bf20df48e495f53
imaginegeek57 <imaginegeek57@gmail.com>
2019-12-30 02:55:33 +0300

    beginning

---
+++ src/main/java/ru/job4j/middle/threads/pinkpong/RectangleMove.java
@@ -0,0 +1,26 @@
+package ru.job4j.middle.threads.pinkpong;
+
+import javafx.scene.shape.Rectangle;
+
+public class RectangleMove implements Runnable {
+
+    private final Rectangle rect;
+
+    public RectangleMove(Rectangle rect) {
+        this.rect = rect;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            this.rect.setX(this.rect.getX() + 1);
+            this.rect.setY(this.rect.getX() - 0.2);
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
