aafc0efd5781b98f3ba5822d9564397805a1d824
imaginegeek57 <imaginegeek57@gmail.com>
2020-01-12 23:49:43 +0300

    4. Скачивание файла с ограничением.[#218284]

--- src/main/java/ru/job4j/middle/multithreading/fileDownload/FileDownload.java
+++ src/main/java/ru/job4j/middle/multithreading/fileDownload/FileDownload.java
@@ -1,22 +1,41 @@
 package ru.job4j.middle.multithreading.fileDownload;
 
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URL;
 
 public class FileDownload {
-    public static void main(String[] args) throws Exception {
-        String file = "https://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml";
-        try (BufferedInputStream in = new BufferedInputStream(new URL(file).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
-            byte dataBuffer[] = new byte[1024];
-            int bytesRead;
-            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-                fileOutputStream.write(dataBuffer, 0, bytesRead);
-                Thread.sleep(1000);
 
+
+    public static void main(String[] args) {
+        new Thread(new MyThread(), "th1").start();
+        new Thread(new MyThread(), "th2").start();
+        new Thread(new MyThread(), "th3").start();
+    }
+
+
+    static class MyThread implements Runnable {
+
+        @Override
+        public void run() {
+            String file = "https://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml";
+            try (BufferedInputStream in = new BufferedInputStream(new URL(file).openStream());
+                 FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+                byte dataBuffer[] = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                    fileOutputStream.write(dataBuffer, 0, bytesRead);
+                    System.out.println("cycle");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
